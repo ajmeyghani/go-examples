f6de60e276eb984e1b5b4577a61fc6345f732384
Amin Meyghani <meyghania@gmail.com>
2017-05-22 11:09:25 -0700

    clean up basics.

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
 import "github.com/aminmeyghani/app1/mlib/math"
 import "github.com/aminmeyghani/app1/mlib/simple"
@@ -9,45 +10,46 @@ import "github.com/aminmeyghani/app1/mlib/basics"
 import "container/list"
 
 func main() {
-  // test the average function
-  xs := []float64{1,2,3,4}
-  avg := math.Average(xs)
-  fmt.Println(avg)
-  // test the simple function that just returns a single float value.
-  fmt.Println(simple.Simple())
-  // test the closure application
-  makeInc := closure.IncMaker()
-  fmt.Println(makeInc())
-  fmt.Println(makeInc())
-  fmt.Println(makeInc())
-  // test the list
-  fmt.Println("............ linkedlist")
-  // Create a new list and put some numbers in it.
-  l := list.New()
-  e4 := l.PushBack(4)
-  e1 := l.PushFront(1)
-  l.InsertBefore(3, e4)
-  l.InsertAfter(2, e1)
+	// test the average function
+	xs := []float64{1, 2, 3, 4}
+	fmt.Println("hello world")
+	avg := math.Average(xs)
+	fmt.Println(avg)
+	// test the simple function that just returns a single float value.
+	fmt.Println(simple.Simple())
+	// test the closure application
+	makeInc := closure.IncMaker()
+	fmt.Println(makeInc())
+	fmt.Println(makeInc())
+	fmt.Println(makeInc())
+	// test the list
+	fmt.Println("............ linkedlist")
+	// Create a new list and put some numbers in it.
+	l := list.New()
+	e4 := l.PushBack(4)
+	e1 := l.PushFront(1)
+	l.InsertBefore(3, e4)
+	l.InsertAfter(2, e1)
 
-  // Iterate through list and print its contents.
-  for e := l.Front(); e != nil; e = e.Next() {
-    fmt.Println(e.Value)
-  }
+	// Iterate through list and print its contents.
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
 
-  // does avo exist
-  fmt.Println(mol.MyConstant)
+	// does avo exist
+	fmt.Println(mol.MyConstant)
 
-  // moles and atoms
-  mol.PrintAll()
+	// moles and atoms
+	mol.PrintAll()
 
-  // print the list of objects
-  objects.PrintCars()
+	// print the list of objects
+	objects.PrintCars()
 
-  // print the list of users
-  objects.PrintUsers()
+	// print the list of users
+	objects.PrintUsers()
 
-  // the basics
-  fmt.Println("BASICS______________________")
-  basics.Eg1()
-  basics.Eg2()
+	// the basics
+	fmt.Println("BASICS______________________")
+	basics.Eg1()
+	basics.Eg2()
 }
--- mlib/basics/basics.go
+++ mlib/basics/basics.go
@@ -1,65 +1,56 @@
 // exploring the basics of the language.
 package basics
+
 import "fmt"
 
 func Eg1() {
-  // type inference, basic assignments
-  var name = "Tom"
-  var triple = [3]int{1,2,3}
-  elms := []string{"tom", "bob", "jon"}
-  isMetal := false
-  fmt.Println(name, triple, elms, isMetal)
+	// type inference, basic assignments
+	var name = "Tom"
+	var triple = [3]int{1, 2, 3}
+	elms := []string{"tom", "bob", "jon"}
+	isMetal := false
+	fmt.Println(name, triple, elms, isMetal)
 }
 
 func Eg2() {
-  // composite types: array, slice, map
-  // slice: variable sizs array
-  type toy struct {
-    id int
-    name string
-  }
-  var toys = []toy {
-    toy{5, "a"},
-    toy{15, "b"},
-  }
+	// composite types: array, slice, map
+	// slice: variable sizs array
+	type toy struct {
+		id   int
+		name string
+	}
+	var toys = []toy{
+		toy{5, "a"},
+		toy{15, "b"},
+	}
 
-  // make a struct and initialize it
-  planet := struct {
-    name string
-    diameter int
-  }{
-    "earth", 12773,
-  }
+	// make a struct and initialize it
+	planet := struct {
+		name     string
+		diameter int
+	}{
+		"earth", 12773,
+	}
 
-  fmt.Println(toys, planet)
+	fmt.Println(toys, planet)
 }
 
-func Obj() {
-  // method is a function attached to a custom type.
-  type amu float64
-  func (mass amu) float() float64 {
-    return float64(mass)
-  }
-}
+// func Obj() {
+//   type amu float64
+//   func (mass amu) float() float64 {
+//     return float64(mass)
+//   }
+// }
 
-
-func InterfaceExample() {
-  type Stringer interface {
-    String() string
-  }
-  // any type that has the method String() attached
-  // automatically implements the Stringer interface.
-  type metal struct {
-    name string
-    number int32
-    weight amu
-  }
-  func (m metalloid) String() string {
-    // ...
-  }
-}
-
-
-
-
-
+// func InterfaceExample() {
+//   type Stringer interface {
+//     String() string
+//   }
+//   type metal struct {
+//     name string
+//     number int32
+//     weight amu
+//   }
+//   func (m metalloid) String() string {
+//   }
+// }
